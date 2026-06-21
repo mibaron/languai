9a3b0009630cf58fe19a1510079e52bf26f4c495
aron <aron.miba@gmail.com>
2026-06-21 18:14:42 +0200

    Update config/settings/production.py default storage

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -51,6 +51,9 @@ LOGGING = {
 # WhiteNoise for static files
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")  # noqa: F405
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
